7532fec96be76db0f49862ed66650fe31802a615
wood0761 <wood0761@gmail.com>
2021-02-19 16:18:40 -0600

    added grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const numCols = 5;
+const numRows = 5;
+
 function App() {
-	return <div>hello cruel world</div>;
+	const grid = [];
+
+	for (let i = 0; i < numRows; i++) {
+		grid.push(Array.from(Array(numCols), () => 0));
+	}
+
+	return (
+		<div
+			style={{
+				display: 'grid',
+				gridTemplateColumns: `repeat(${numCols}, 30px)`,
+			}}
+		>
+			{grid.map((rows, i) =>
+				rows.map((cols, k) => {
+					return (
+						<div style={{ height: 30, width: 30, border: '1px solid black' }} />
+					);
+				})
+			)}
+		</div>
+	);
 }
 
 export default App;
