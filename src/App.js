ccd79d992428832f52632656b8ce7243fe1cf734
wood0761 <wood0761@gmail.com>
2021-02-22 22:03:29 -0600

    food score speed-increase no-backwards eat-yourself

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import { useState, useEffect } from 'react';
 import { useInterval } from './tools/useInterval';
 
-const numCols = 5;
-const numRows = 5;
+const numCols = 20;
+const numRows = 10;
+
+const getRandomCoords = () => {
+	return [
+		Math.floor(Math.random() * numRows),
+		Math.floor(Math.random() * numCols),
+	];
+};
+
+const arrayEquality = (a, b) => {
+	return a.every((val, index) => val === b[index]);
+};
+
+const checkAteYourself = (a, b) => {
+	return b.some(array => arrayEquality(a, array));
+};
 
 function App() {
 	const gridConstant = [];
@@ -12,6 +27,10 @@ function App() {
 	const [snake, setSnake] = useState([[0, 0]]);
 	const [direction, setDirection] = useState('RIGHT');
 	const [grid, setGrid] = useState(gridConstant);
+	const [food, setFood] = useState(getRandomCoords);
+	const [paused, setPaused] = useState(true);
+	const [delay, setDelay] = useState(200);
+	const [score, setScore] = useState(0);
 
 	const moveSnake = () => {
 		const newSnake = [...snake];
@@ -36,8 +55,8 @@ function App() {
 					} else {
 						piece = [piece[0], piece[1] - 1];
 					}
+					newSnake.unshift(piece);
 				}
-				newSnake.unshift(piece);
 			});
 		}
 		if (direction === 'UP') {
@@ -48,8 +67,8 @@ function App() {
 					} else {
 						piece = [piece[0] - 1, piece[1]];
 					}
+					newSnake.unshift(piece);
 				}
-				newSnake.unshift(piece);
 			});
 		}
 		if (direction === 'DOWN') {
@@ -60,37 +79,60 @@ function App() {
 					} else {
 						piece = [piece[0] + 1, piece[1]];
 					}
+					newSnake.unshift(piece);
 				}
-				newSnake.unshift(piece);
 			});
 		}
 
-		newSnake.pop();
+		if (!arrayEquality(newSnake[0], food)) {
+			newSnake.pop();
+		} else {
+			setFood(getRandomCoords);
+			setDelay(delay * 0.95);
+			setScore(score + 1);
+		}
 		const newGrid = [...gridConstant];
 		newSnake.forEach(piece => {
 			newGrid[piece[0]][piece[1]] = 1;
 		});
+
+		if (checkAteYourself(newSnake[0], newSnake.slice(1))) {
+			setPaused(true);
+		}
+
+		newGrid[food[0]][food[1]] = 1;
 		setSnake(newSnake);
 		setGrid(newGrid);
 	};
 
-	useInterval(() => {
-		moveSnake();
-	}, 1000);
+	useInterval(
+		() => {
+			moveSnake();
+		},
+		paused ? null : delay
+	);
 
 	const handleKeydown = event => {
 		switch (event.keyCode) {
 			case 37:
-				setDirection('LEFT');
+				if (direction !== 'RIGHT') {
+					setDirection('LEFT');
+				}
 				break;
 			case 38:
-				setDirection('UP');
+				if (direction !== 'DOWN') {
+					setDirection('UP');
+				}
 				break;
 			case 39:
-				setDirection('RIGHT');
+				if (direction !== 'LEFT') {
+					setDirection('RIGHT');
+				}
 				break;
 			case 40:
-				setDirection('DOWN');
+				if (direction !== 'UP') {
+					setDirection('DOWN');
+				}
 				break;
 			default:
 				setDirection('RIGHT');
@@ -103,28 +145,34 @@ function App() {
 	});
 
 	return (
-		<div
-			style={{
-				display: 'grid',
-				gridTemplateColumns: `repeat(${numCols}, 30px)`,
-				justifyContent: 'center',
-			}}
-		>
-			{grid.map((rows, i) =>
-				rows.map((cols, k) => {
-					return (
-						<div
-							style={{
-								height: 30,
-								width: 30,
-								border: '1px solid black',
-								background: grid[i][k] ? 'black' : null,
-							}}
-						/>
-					);
-				})
-			)}
-		</div>
+		<>
+			<button onClick={() => setPaused(!paused)}>
+				{paused ? 'PLAY' : 'PAUSE'}
+			</button>
+			SCORE: {score}
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: `repeat(${numCols}, 30px)`,
+					justifyContent: 'center',
+				}}
+			>
+				{grid.map((rows, i) =>
+					rows.map((cols, k) => {
+						return (
+							<div
+								style={{
+									height: 30,
+									width: 30,
+									border: '1px solid black',
+									background: grid[i][k] ? 'black' : null,
+								}}
+							/>
+						);
+					})
+				)}
+			</div>
+		</>
 	);
 }
 
